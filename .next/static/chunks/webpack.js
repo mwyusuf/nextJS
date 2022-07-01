fa1ed87bab850e611bf73aace0084c2eb4be0ea8
mwyusuf <yusuf.bagus@machtwatch.co.id>
2022-07-01 19:36:24 +0700

    navigation

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7ed0e2ff803ff691"; }
+/******/ 		__webpack_require__.h = function() { return "07b0ebddc06f50bc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/notes/index.jsx
+++ pages/notes/index.jsx
@@ -1,3 +1,9 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
 
-export default () => <h1>Notes</h1>
+export default () => <>
+    <h1>Notes</h1>
+    <Link href="/notes/[id]" as={`notes/1`}>
+        <a>notes</a>
+    </Link>
+</>
